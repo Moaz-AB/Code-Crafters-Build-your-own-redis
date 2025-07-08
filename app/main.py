be17833b51bb712ed1b757f6efcef6ef2759e082
Moaz Abuzaid <moazabuzaid9@gmail.com>
2025-07-08 13:11:39 -0400

    Stage 4: Added multi-client support with threads

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 import socket
+import threading
 
-def main():
-    print("Server starting...")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-    print("Client connected.")
-
+def handle_client(connection):
     while True:
         data = connection.recv(1024)
         if not data:
-            break  # connection closed
-        print(f"Received: {data}")
+            break
         connection.sendall(b"+PONG\r\n")
+    connection.close()
+
+def main():
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    print("Server is running and waiting for clients...")
+
+    while True:
+        client_conn, _ = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_conn,))
+        client_thread.start()
 
 if __name__ == "__main__":
     main()
+
